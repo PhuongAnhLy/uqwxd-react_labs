5a4ee6f957001d1d4c040d37d7e85d5fb375023e
PhuongAnhLy <phuonganh.pal27@gmail.com>
2023-10-25 00:04:13 -0400

    update

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -4,7 +4,22 @@ const App = () => {
   const [todos, setTodos] = useState([]);
   
   // Add the handlesubmit code here
-  
+  function handleSubmit(e) {
+      e.preventDefault();
+
+      let todo = document.getElementById('todoAdd').value
+      const newTodo = {
+          id: new Date().getTime(),
+          text: todo.trim(),
+          completed: false,
+      };
+      if (newTodo.text.length > 0) {
+          setTodos([...todos].concat(newTodo));
+      } else {
+          alert("Enter Valid Task");
+      }
+      document.getElementById('todoAdd').value = ""
+  }
   
   // Add the deleteToDo code here
 
@@ -15,14 +30,21 @@ const App = () => {
   // Add the submitEdits code here
 
   
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" id= 'todoAdd'/>
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+    return (
+        <div id="todo-list">
+            <h1>Todo List</h1>
+                <form onSubmit={handleSubmit}>
+                    <input 
+                        type ="text" 
+                        align ="right" 
+                        id= 'todoAdd'
+                    />
+                    <button type ="submit">Add Todo</button>
+                </form>
+                {todos.map((todo) => <div className="todo" key={todo.id}>
+                    <div className="todo-text">{todo.text}</div>    
+                </div>)}
+        </div>
+    );
 };
 export default App;
